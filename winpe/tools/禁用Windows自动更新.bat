8c257d2c752d8549a4590c336342993dac4239f4
shmilee <shmilee.zju@gmail.com>
2024-09-05 13:17:54 +0800

    add/update winpe tools
    - BOOTICE
    - Dism
    - WinNTSetup
    - 禁用Windows自动更新.bat

---
+++ winpe/tools/禁用Windows自动更新.bat
@@ -0,0 +1,31 @@
+@echo off
+
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+    pushd "%CD%"
+    CD /D "%~dp0"
+
+REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" /v NoAutoUpdate /t REG_DWORD /d 1 /f
+
+sc config wuauserv start= disabled
+
+echo.
+echo 操作成功完成，重启后生效
+
+pause
